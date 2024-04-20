7dd75323a3cf21af4092afede3f3dc533962cc3a
KsusaArsyukhina <Arsyukhina03@mail.com>
2024-04-20 14:19:46 +0300

    summary v1

---
+++ routes/games.js
@@ -0,0 +1,15 @@
+// routes/games.js
+const gamesRouter = require("express").Router();
+const {
+  sendAllGames,
+  deleteGame,
+  addGameController,
+} = require("../controllers/games");
+
+const { getAllGames } = require("../middlewares/games");
+
+gamesRouter.get("/games", getAllGames, sendAllGames);
+gamesRouter.delete("/games/:id", getAllGames, deleteGame);
+gamesRouter.post("/games", getAllGames, addGameController);
+
+module.exports = gamesRouter;
